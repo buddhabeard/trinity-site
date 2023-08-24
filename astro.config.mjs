4583897016c8c2fcabe177dbc5a210c11895c189
Josh Waller <mdx.programs@gmail.com>
2023-08-24 13:23:21 -0400

    configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://trinitylifeministry.com",
+  integrations: [tailwind(), react(), sitemap()]
+});
