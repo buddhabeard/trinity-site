fb486d335b6562207e3eb5bd9c8df0dc3fa6912a
Josh Waller <mdx.programs@gmail.com>
2023-08-24 13:43:20 -0400

    adds robotstxt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 
+import robotsTxt from "astro-robots-txt";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://trinitylifeministry.com",
-  integrations: [tailwind(), react(), sitemap()]
+  integrations: [tailwind(), react(), sitemap(), robotsTxt()]
 });
