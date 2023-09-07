83c13a2ae4973ed2abdb4b5004d058e7841dc5a8
Josh Waller <mdx.programs@gmail.com>
2023-09-07 12:37:33 -0400

    port component and fix styles, adds query helper

---
+++ src/lib/queries.ts
@@ -0,0 +1,24 @@
+// const test = ["hero", "linkCards|>linkCard", "faqs", "callout|>image", "cta"];
+
+export const createQuery = (queries: string[]) =>
+  queries
+    .map((t) => {
+      if (!t.includes("|>")) return `populate[${t}][populate]=*`;
+      return (
+        "populate" +
+        t
+          .split("|>")
+          .map((tn) => `[${tn}]`)
+          .join("[populate]") +
+        "[populate]=*"
+      );
+    })
+    .join("&");
+
+export const homepageQuery = createQuery([
+  "hero",
+  "linkCards|>linkCard",
+  "faqs",
+  "callout|>image",
+  "cta",
+]);
