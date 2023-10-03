cd3c45ea4ac563e2259c83dd3fff8ed0d7a38f60
Josh Waller <mdx.programs@gmail.com>
2023-10-03 10:52:11 -0400

    AcceptingDonations fixup

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
+      },
       colors: ({ colors }) => ({
         ...colors,
         teal: "#13A89E",
