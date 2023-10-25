433ac30d96f09d8c78fe96655a4ee59542ec8dcf
bfaulk04 <bfaulk04@gmail.com>
2023-10-25 12:18:56 -0400

    idk I hope nothing breaks

--- src/components/Pill.tsx
+++ src/components/Pill.tsx
@@ -18,7 +18,7 @@ const Pill = ({
 }: PillProps) => (
   <button
     onClick={onClick}
-    className={`inline-flex items-center rounded-full px-3 py-0.5 text-sm font-medium bg-${bg} text-${color} ${classes}`}
+    className={`inline-flex items-center rounded-full px-6 py-2 text-sm font-medium bg-${bg} text-${color} ${classes}`}
   >
     {children}
   </button>
