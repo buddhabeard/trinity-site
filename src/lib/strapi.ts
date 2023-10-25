ebd9d42fac88fe0c98aef333dbdd243f23e22368
Josh Waller <mdx.programs@gmail.com>
2023-10-25 13:17:06 -0400

    fixes sort on post query for home blog cards

--- src/lib/strapi.ts
+++ src/lib/strapi.ts
@@ -3,6 +3,7 @@ interface Props {
   query?: Record<string, string> | string;
   wrappedByKey?: string;
   wrappedByList?: boolean;
+  sortBy?: string;
 }
 
 /**
@@ -11,6 +12,7 @@ interface Props {
  * @param query - The query parameters to add to the url
  * @param wrappedByKey - The key to unwrap the response from
  * @param wrappedByList - If the response is a list, unwrap it
+ * @param sortBy - key to sort by
  * @returns
  */
 export default async function fetchApi<T>({
@@ -18,12 +20,15 @@ export default async function fetchApi<T>({
   query,
   wrappedByKey,
   wrappedByList,
+  sortBy = "",
 }: Props): Promise<T> {
   if (endpoint.startsWith("/")) {
     endpoint = endpoint.slice(1);
   }
 
-  let url = `${import.meta.env.STRAPI_URL}/api/${endpoint}?${query}`;
+  let url = `${
+    import.meta.env.STRAPI_URL
+  }/api/${endpoint}?${query}&sort=${sortBy}`;
 
   const res = await fetch(url);
   let data = await res.json();
