9603cc16eed3f3d555708527a8420f499326d6df
Josh Waller <mdx.programs@gmail.com>
2023-10-25 22:49:18 -0400

    more style fixes

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -73,7 +73,7 @@ const ContactForm = () => {
   return (
     <form
       id="contact-form"
-      className="w-full shadow p-4 mt-10"
+      className="w-full shadow px-4 mt-8 lg:mt-10"
       onSubmit={handleSubmit}
     >
       <div className="flex flex-col justify-center pt-4">
@@ -173,7 +173,7 @@ const ContactForm = () => {
                 alt=""
               />
 
-              <div className="my-6 flex items-center justify-start gap-x-6">
+              <div className="mb-8 lg:my-6 flex items-center justify-start gap-x-6">
                 <button
                   type="submit"
                   className="rounded-md bg-teal px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
