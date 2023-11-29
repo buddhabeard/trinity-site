c44b1ad462a5880dd56c65e373cc76a9c5457ada
Josh Waller <mdx.programs@gmail.com>
2023-11-29 12:19:40 -0500

    fixes class to className for tsx component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,7 @@
 import logoImg from "../images/logo.svg";
 
 const Logo = () => (
-  <div class="h-full w-full p-2">
+  <div className="h-full w-full p-2">
     <img src={logoImg} alt="logo" />
   </div>
 );
